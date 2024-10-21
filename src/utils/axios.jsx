5a73e2016cd6676725f1ff41a64d87b033912b2f
sharmaanmol256 <49368500+sharmaanmol256@users.noreply.github.com>
2024-10-21 22:26:13 +0530

    Update axios.jsx

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -4,8 +4,7 @@ const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3/",
     headers: {
         accept: "application/json",
-        Authorization: import.meta.env.VITE_REACT_APP_TOKEN,
-    },
-});
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZjNmYWYwM2RmZTEzYzk0NTg5ZTllY2M0ZDZlMjYxZiIsIm5iZiI6MTcyOTUyOTA1NC4xNDc3OTMsInN1YiI6IjY2ZDcyYTBkNTdiM2Y1YTVjOWY3MTQ2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LPu751BCwI55N9vNVDP8AEBNM_o4A9U31Oc779mHC-A'
+}});
 
 export default instance;
