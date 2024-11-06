11388ba0f113a3cd56b254b142279200ac42665a
sharmaanmol256 <49368500+sharmaanmol256@users.noreply.github.com>
2024-11-07 00:10:48 +0530

    Update Sidenav.jsx

--- src/components/partials/Sidenav.jsx
+++ src/components/partials/Sidenav.jsx
@@ -1,14 +1,15 @@
-import axios from "../../utils/axios";
-import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Sidenav = () => {
     return (
-        <div className="w-[20%] h-full  border-r-2 border-zinc-400 p-10">
+        <div className="h-full bg-black p-5 lg:p-10">
+            {/* Logo */}
             <h1 className="text-2xl text-white font-bold">
                 <i className="text-[#6556CD] ri-tv-fill mr-2"></i>
                 <span className="text-2xl">SCSDB.</span>
             </h1>
+
+            {/* New Feeds Navigation */}
             <nav className="flex flex-col text-zinc-400 text-xl gap-3">
                 <h1 className="text-white font-semibold text-xl mt-10 mb-5">
                     New Feeds
@@ -48,7 +49,11 @@ const Sidenav = () => {
                     People
                 </Link>
             </nav>
-            <hr className="border-none h-[1px] bg-zinc-400" />
+
+            {/* Divider */}
+            <div className="my-4 h-[1px] bg-zinc-800" />
+
+            {/* Website Information Navigation */}
             <nav className="flex flex-col text-zinc-400 text-xl gap-3">
                 <h1 className="text-white font-semibold text-xl mt-10 mb-5">
                     Website Information
